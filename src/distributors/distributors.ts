c36434e55c21ca57a3b6deda2976b4f9a4b31299
Julien-devatom <julien@morpho.xyz>
2024-04-10 12:12:21 +0200

    feat: clean points state

--- src/distributors/distributors.ts
+++ src/distributors/distributors.ts
@@ -10,6 +10,82 @@ import { getConfig } from "../index";
 
 const distributedStateCache = new Map<number, PointsState>();
 
+const cleanPointsState = (state: State): PointsState => {
+  const markets = mapValues(
+    state.markets,
+    ({
+      totalSupplyPoints,
+      totalCollateralPoints,
+      totalBorrowPoints,
+      totalSupplyShards,
+      totalCollateralShards,
+      totalBorrowShards,
+      id,
+      loanToken,
+      collateralToken,
+    }) => ({
+      totalSupplyPoints,
+      totalCollateralPoints,
+      totalBorrowPoints,
+      totalSupplyShards,
+      totalCollateralShards,
+      totalBorrowShards,
+      id,
+      loanToken,
+      collateralToken,
+    })
+  );
+
+  const positions = mapValues(
+    state.positions,
+    ({
+      id,
+      supplyPoints,
+      borrowPoints,
+      collateralPoints,
+      supplyShards,
+      borrowShards,
+      collateralShards,
+      market,
+      user,
+    }) => ({
+      id,
+      supplyPoints,
+      borrowPoints,
+      collateralPoints,
+      supplyShards,
+      borrowShards,
+      collateralShards,
+      market,
+      user,
+    })
+  );
+
+  const metaMorphos = mapValues(state.metaMorphos, ({ totalPoints, totalShards, id }) => ({
+    totalPoints,
+    totalShards,
+    id,
+  }));
+
+  const metaMorphoPositions = mapValues(
+    state.metaMorphoPositions,
+    ({ id, supplyPoints, supplyShards, metaMorpho, user }) => ({
+      id,
+      supplyPoints,
+      supplyShards,
+      metaMorpho,
+      user,
+    })
+  );
+
+  return {
+    markets,
+    positions,
+    metaMorphos,
+    metaMorphoPositions,
+  };
+};
+
 export const distributeUpTo = (state: State, timestamp: bigint): PointsState => {
   const config = getConfig();
   if (config.cacheEnabled && distributedStateCache.has(Number(timestamp))) {
@@ -29,12 +105,12 @@ export const distributeUpTo = (state: State, timestamp: bigint): PointsState => 
   const metaMorphoPositions = mapValues(state.metaMorphoPositions, (position) =>
     computeMetaMorphoPositionPoints(metaMorphos[position.metaMorpho]!, position, timestamp)
   );
-  const snapshot = {
+  const snapshot = cleanPointsState({
     markets,
     positions,
     metaMorphos,
     metaMorphoPositions,
-  };
+  });
 
   if (config.cacheEnabled) {
     distributedStateCache.set(Number(timestamp), snapshot);
