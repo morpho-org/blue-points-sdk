b8de773b345272d6edb66e2641cf9601f1464bd6
Julien-devatom <julien@morpho.xyz>
2024-04-14 16:52:37 +0200

    refactor(redistribution) remove redistribution to vault collateral users
    This is now handled directly by the subgraph

--- src/distributors/redistributor.ts
+++ src/distributors/redistributor.ts
@@ -3,7 +3,6 @@ import { Address, Hex } from "viem";
 import { blacklistingAddress, ShardsState, cloneShardsState } from "..";
 
 import { MORPHO_ADDRESS } from "./constants";
-import { getMetaMorphoPositionId, initMetaMorphoPointsPosition } from "./metaMorphoDistributor";
 import { getPositionId, initPositionShards } from "./morphoDistributor";
 import { freemmer } from "./utils";
 
@@ -77,100 +76,10 @@ export const redistributeMetaMorpho = (state: ShardsState): ShardsState =>
     cloneShardsState(state)
   );
 
-const redistributeVaultShardsToCollateralUsers = (
-  state: ShardsState,
-  mmAddress: Address,
-  marketId: Hex,
-  shardsToRedistribute: bigint
-): ShardsState => {
-  const market = state.markets[marketId]!;
-
-  if (market.totalCollateralShards === 0n) return state;
-  let totalShardsRedistributed = 0n;
-
-  const newState = Object.values(state.positions)
-    .filter(({ market, collateralShards }) => market === marketId && collateralShards !== 0n)
-    .reduce((resultingState, { user, collateralShards }) => {
-      const userVaultShards =
-        (shardsToRedistribute * collateralShards) / market.totalCollateralShards;
-
-      const vaultPositionId = getPositionId(marketId, user);
-      const userVaultPosition =
-        state.metaMorphoPositions[vaultPositionId] ?? initMetaMorphoPointsPosition(mmAddress, user);
-      userVaultPosition.supplyShards += userVaultShards;
-
-      totalShardsRedistributed += userVaultShards;
-
-      resultingState.metaMorphoPositions[vaultPositionId] = userVaultPosition;
-      return resultingState;
-    }, state);
-
-  const mmBluePosition =
-    newState.metaMorphoPositions[getMetaMorphoPositionId(mmAddress, MORPHO_ADDRESS)]!;
-  mmBluePosition.supplyShards -= totalShardsRedistributed;
-
-  if (mmBluePosition.supplyShards < 0n) {
-    throw new Error(`Negative Morpho supply shards or points for metamorpho ${mmAddress}`);
-  }
-
-  return newState;
-};
-
-const redistributeCollateralPointsToMetaMorphoUsers = (
-  state: ShardsState,
-  mmAddress: Address
-): ShardsState => {
-  const vaultBluePosition =
-    state.metaMorphoPositions[getMetaMorphoPositionId(mmAddress, MORPHO_ADDRESS)];
-  if (!vaultBluePosition) return state;
-
-  //TODO: what if a vault is added as loan asset in a market?
-  const totalVaultShardsOnBlue = vaultBluePosition.supplyShards;
-
-  const marketsWithVaultAsCollateral = Object.values(state.markets).filter(
-    ({ collateralToken }) => collateralToken === mmAddress
-  );
-  const allMarketsCollateralShards = marketsWithVaultAsCollateral.reduce(
-    (acc, { totalCollateralShards }) => acc + totalCollateralShards,
-    0n
-  );
-  if (allMarketsCollateralShards === 0n) return state;
-
-  return marketsWithVaultAsCollateral.reduce((resultingState, { totalCollateralShards, id }) => {
-    const vaultShardsToRedistribute =
-      (totalVaultShardsOnBlue * totalCollateralShards) / allMarketsCollateralShards;
-
-    return redistributeVaultShardsToCollateralUsers(
-      resultingState,
-      mmAddress,
-      id,
-      vaultShardsToRedistribute
-    );
-  }, cloneShardsState(state));
-};
-
-/** Redistribute the points accrued by Morpho as vault user to the different markets users
- *
- * Map over all vaults that are used as collateral.
- * */
-export const redistributeVaultAsCollateral = (state: ShardsState): ShardsState => {
-  const vaultAsCollateral = Object.values(state.metaMorphos).filter(({ id }) =>
-    Object.values(state.markets).some(
-      ({ collateralToken, totalCollateralShards }) =>
-        collateralToken === id && totalCollateralShards > 0n
-    )
-  );
-
-  return vaultAsCollateral.reduce(
-    (resultingState, { id }) => redistributeCollateralPointsToMetaMorphoUsers(resultingState, id),
-    cloneShardsState(state)
-  );
-};
-
 /**
  * We first need to redistribute to the users that have used the vault as collateral
  * And then, we spread the vault market points to the vault users.
  * Finally, we blacklist the remaining points of blue (due to roundings during the redistribution)
  */
 export const redistributeAll = (state: ShardsState) =>
-  blacklistingAddress(redistributeMetaMorpho(redistributeVaultAsCollateral(state)), MORPHO_ADDRESS);
+  blacklistingAddress(redistributeMetaMorpho(state), MORPHO_ADDRESS);
